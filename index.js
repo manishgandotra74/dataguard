ae76ce67680a7ef2929b0ed8c609cf12540eee27
Ethan Hunt <manishgandotra74@gmail.com>
2022-04-28 02:07:39 +0530

    changes

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const path = require('path');
 const express = require('express');
 const app = express()
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, './build')));
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
-app.listen(process.env.PORT || 7000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
