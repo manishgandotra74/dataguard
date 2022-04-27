401b70d51f4e908a4a146cd1ac69bea9d690267c
Ethan Hunt <manishgandotra74@gmail.com>
2022-04-28 02:28:13 +0530

    changes

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
 });
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port);
 });
