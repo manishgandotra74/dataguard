4b00a41fb301e19f6f24e7e96d67f50e4de9bb7d
Ethan Hunt <manishgandotra74@gmail.com>
2022-04-27 20:31:51 +0530

    builf

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express()
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, './client/build')));
+app.use(express.static(path.resolve(__dirname, './build')));
 
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './build', 'index.html'));
 });
 app.listen(7000)
