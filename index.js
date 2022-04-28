ff4d0fb65fc38a2f4b9289cb6fdb09cce241cfdf
Ethan Hunt <manishgandotra74@gmail.com>
2022-04-28 14:31:08 +0530

    changes

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 var os = require("os");
-console.log(os.hostname());
+console.log('os', os.hostname());
 server.use(middlewares);
 server.use(router);
 
-server_json = server.listen( 3001, function(){
-  var host = server_json.address().address;
-  var port = server_json.address().port;
-  console.log('running at http://' + host + ':' + port)});
 const app = express()
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, './build')));
@@ -20,12 +16,11 @@ app.use(express.static(path.resolve(__dirname, './build')));
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-  console.log(req.hostname);
+  console.log('hostname',req.hostname);
   res.sendFile(path.resolve(__dirname, './build', 'index.html'));
 });
 
-var server_express = app.listen(process.env.PORT || 3000, function () {
-  var host = server_express.address().address;
-  var port = server_express.address().port;
-  console.log('running at http://' + host + ':' + port)
+app.listen(process.env.PORT || 3000, function () {
+
+  server.listen(3001)
 });
